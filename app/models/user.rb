d518b98b603944cfdaad32d3101914eb35ea2f05
Luciana Sugeta <lulusugeta@gmail.com>
2021-05-27 21:20:13 -0400

    commit tree 2b1fa8ff9c814b138cc75ea12e813c7c31a14255

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+  validates :email, :password, presence: true
+  validates :username, uniqueness: true
+
+  def slug
+    username.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find {|username| username.slug == slug}
+  end
 end
